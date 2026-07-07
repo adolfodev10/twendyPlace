01c280561ccd4c77d4b8507b24e5366091c3ec48
adolfodev10 <adolfomanueldev10@gmail.com>
2026-07-07 07:50:09 +0100

    fix: update AdminDashboard routing to render Dashboard component instead of AdminDashboard

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -5,13 +5,14 @@ import OrdersManager from '../components/admin/OrdersManager';
 import ProductsManager from '../components/admin/ProductsManager';
 import ClientsManager from '../components/admin/ClientsManager';
 import ProtectedRoute from '../components/common/ProtectedRoute';
+import Dashboard from '../components/admin/Dashboard';
 
 const AdminDashboard: React.FC = () => {
   return (
     <ProtectedRoute requireAdmin>
       <AdminLayout>
         <Routes>
-          <Route path="/" element={<AdminDashboard />} />
+          <Route path="/" element={<Dashboard />} />
           <Route path="/orders" element={<OrdersManager />} />
           <Route path="/products" element={<ProductsManager />} />
           <Route path="/clients" element={<ClientsManager />} />
