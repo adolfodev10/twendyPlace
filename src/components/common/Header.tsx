b014e7db1057ef65d6bd31ac562bca2e0657dccd
adolfodev10 <adolfomanueldev10@gmail.com>
2026-08-19 16:53:23 +0100

    fix: remove unnecessary console log for logo error in Header component

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -59,6 +59,8 @@ const Header: React.FC<HeaderProps> = ({ cartCount, onCartClick, user }) => {
     setIsUserDropdownOpen(false);
   };
 
+  console.log(logoError, 'logoError');
+
   return (
     <header className="bg-white border-b border-gray-200 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
